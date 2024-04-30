da48102c23e2ff6f0148881faa10f2eb2a154fc5
Michael Purtill <michaelpurtill123@hotmail.com>
2024-04-29 22:41:06 -0300

    some basic command line ops

--- source/app.d
+++ source/app.d
@@ -3,16 +3,33 @@ import std.format;
 import std.json;
 import requests;
 import std.algorithm;
+import std.getopt;
 
-void main()
+const string apiUrl = "https://tatoeba.org/en/api_v0/search";
+const string audioApiUrl = "https://tatoeba.org/en/audio/download/";
+
+void main(string[] args)
 {
+	// API OPTIONS
+	string langFrom = "";
+	string langTo = "";
+	bool hasAudio = false;
 	long pageCount = 0;
-	const int perPage = 10; // this is the default perPage value in tatoeba's api.
 
-	const string apiUrl = "https://tatoeba.org/en/api_v0/search";
-	const string audioApiUrl = "https://tatoeba.org/en/audio/download/";
+	getopt(args,
+		"l|lang", &langFrom,
+		"a|audio", &hasAudio
+	);
 
-	auto statsRes = getContent(apiUrl, ["from": "jpn", "has_audio": "yes", "query": "", "to": "", "page": "1"]);
+	auto statsRes = getContent(apiUrl, [
+			"from": langFrom,
+			"has_audio": hasAudio ? "yes": "no",
+			"query": "",
+			"to": "",
+			"page": "1" // seems like tatoeba's api is 1-indexed.
+		]);
+	
+	writeln(statsRes.toString());
 
 	JSONValue statsJson = parseJSON(statsRes.toString());
 
@@ -20,22 +37,22 @@ void main()
 
 	pageCount = paging["Sentences"]["pageCount"].integer;
 
-	char[] csvString = cast (char[]) "Sentence, Audio,\n".idup();
+	// char[] csvString = cast (char[]) "Sentence, Audio,\n".idup();
 
 	foreach (i; 1..(pageCount + 1)) {
-		auto res = getContent(apiUrl, ["from": "jpn", "has_audio": "yes", "query": "", "to": "", "page": "%d".format(i)]);
+	// 	auto res = getContent(apiUrl, ["from": "jpn", "has_audio": "yes", "query": "", "to": "", "page": "%d".format(i)]);
 
-		JSONValue jsonContent = parseJSON(res.toString());
+	// 	JSONValue jsonContent = parseJSON(res.toString());
 
-		JSONValue sentences = jsonContent["results"];
+	// 	JSONValue sentences = jsonContentget["results"];
 
-		foreach (s; sentences.array()) {
-			string text = s["text"].str;
-			long audioId = s["audios"][0]["id"].integer;
-			string audioUrl = "%s%d".format(audioApiUrl, audioId);
-			csvString ~= "%s,%s,\n".format(text, audioUrl);
-		}
+	// 	foreach (s; sentences.array()) {
+	// 		string text = s["text"].str;
+	// 		long audioId = s["audios"][0]["id"].integer;
+	// 		string audioUrl = "%s%d".format(audioApiUrl, audioId);
+	// 		csvString ~= "%s,%s,\n".format(text, audioUrl);
+	// 	}
 	}
 
-	writeln(csvString);
+	writeln(pageCount);
 }
