12bd4354720a855096a63b85a24f71eabf92b459
Michael Purtill <michaelpurtill123@hotmail.com>
2024-04-30 21:04:43 -0300

    accept command line args, do directory/file io to store results

--- source/app.d
+++ source/app.d
@@ -4,21 +4,24 @@ import std.json;
 import requests;
 import std.algorithm;
 import std.getopt;
+import std.file;
 
 const string apiUrl = "https://tatoeba.org/en/api_v0/search";
 const string audioApiUrl = "https://tatoeba.org/en/audio/download/";
 
-void main(string[] args)
+int main(string[] args)
 {
 	// API OPTIONS
 	string langFrom = "";
 	string langTo = "";
 	bool hasAudio = false;
 	long pageCount = 0;
+	string directory = "./tatoeba_results";
 
 	getopt(args,
 		"l|lang", &langFrom,
-		"a|audio", &hasAudio
+		"a|audio", &hasAudio,
+		"d|destination", &directory
 	);
 
 	auto statsRes = getContent(apiUrl, [
@@ -28,8 +31,6 @@ void main(string[] args)
 			"to": "",
 			"page": "1" // seems like tatoeba's api is 1-indexed.
 		]);
-	
-	writeln(statsRes.toString());
 
 	JSONValue statsJson = parseJSON(statsRes.toString());
 
@@ -37,22 +38,41 @@ void main(string[] args)
 
 	pageCount = paging["Sentences"]["pageCount"].integer;
 
-	// char[] csvString = cast (char[]) "Sentence, Audio,\n".idup();
-
-	foreach (i; 1..(pageCount + 1)) {
-	// 	auto res = getContent(apiUrl, ["from": "jpn", "has_audio": "yes", "query": "", "to": "", "page": "%d".format(i)]);
-
-	// 	JSONValue jsonContent = parseJSON(res.toString());
-
-	// 	JSONValue sentences = jsonContentget["results"];
-
-	// 	foreach (s; sentences.array()) {
-	// 		string text = s["text"].str;
-	// 		long audioId = s["audios"][0]["id"].integer;
-	// 		string audioUrl = "%s%d".format(audioApiUrl, audioId);
-	// 		csvString ~= "%s,%s,\n".format(text, audioUrl);
-	// 	}
+	try
+	{
+		mkdir(directory);
+	}
+	catch (Exception e)
+	{
+		writeln("couldn't create directory %s".format(directory));
+		return 1;
 	}
 
-	writeln(pageCount);
+	foreach (i; 1 .. (pageCount + 1))
+	{
+		auto res = getContent(apiUrl, [
+				"from": "jpn",
+				"has_audio": "yes",
+				"query": "",
+				"to": "",
+				"page": "%d".format(i)
+			]);
+
+		JSONValue jsonContent = parseJSON(res.toString());
+
+		JSONValue sentences = jsonContent["results"];
+
+		foreach (s; sentences.array())
+		{
+			string text = s["text"].str;
+			long audioId = s["audios"][0]["id"].integer;
+			string audioUrl = "%s%d".format(audioApiUrl, audioId);
+
+			File file = File(directory ~ "/" ~ text ~ ".txt", "w+");
+
+			file.writeln(text);
+		}
+	}
+
+	return 0;
 }
